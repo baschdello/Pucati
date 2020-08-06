b6fe39b8806190bd09dea04c078f1655a77e6511
Bastian Urschel <bastian@namda>
2020-08-06 18:59:36 +0200

    Fixed input levels

--- Pucati/src/main.cpp
+++ Pucati/src/main.cpp
@@ -5,6 +5,7 @@
 #define FLOATSWITCH   PB4
 
 volatile uint8_t lock;
+volatile uint8_t time;
 void timerISR();
 Ticker timer(timerISR, 1000);
 
@@ -15,25 +16,26 @@ void setup() {
   pinMode(PB2,INPUT_PULLUP);
   digitalWrite(WATERPUMP,HIGH);
   pinMode(FLOATSWITCH,INPUT_PULLUP);
+  //timer.interval(1000); //ms
   timer.stop();
   wdt_enable(WDTO_1S);
 }
 
 void timerISR() {
-  pinMode(WATERPUMP,INPUT);
-  timer.stop();
-  lock = 0;
+  if(time-- == 0) {
+    pinMode(WATERPUMP,INPUT);
+    timer.stop();
+    lock = 0;
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  uint8_t time;
 
   if(!digitalRead(FLOATSWITCH) && !lock)
   {
-    time = digitalRead(PB2)*40 + digitalRead(PB1)*20 + digitalRead(PB0)*10;
+    time = !digitalRead(PB2)*40 + !digitalRead(PB1)*20 + !digitalRead(PB0)*10;
     pinMode(WATERPUMP,OUTPUT);
-    timer.interval(time*1000); //ms
     timer.start();
     lock = 1;
   }
